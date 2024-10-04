75d445b22094500a49f2e094ed5c2a8f4e9343be
billylin609 <linyuchen830@gmail.com>
2024-10-04 09:04:29 -0400

    test windows pcan driver

--- can_parser_py/python-can.py
+++ can_parser_py/python-can.py
@@ -1,4 +1,15 @@
+#setup instructions: 
+# https://canable.io/getting-started.html
+# https://pemd-sys.github.io/posts/Can-Comms-python/
+
 import can
+
+#try candlelight firmware for linux dev
+
+#candlelight firmware
+#bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
+
+#jetson can enable: https://forums.developer.nvidia.com/t/how-to-use-can-on-jetson-tx2/54125
 
 # Stock slcan firmware on Windows
 bus = can.interface.Bus(interface='pcan', bitrate=100000)
